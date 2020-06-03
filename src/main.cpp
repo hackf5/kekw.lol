86a3f3f7f9b6c791bbe1eebbd517eb1cb7beba32
Brian Tyler <brian@hackf5.io>
2020-06-03 23:19:57 +0100

    initial revision

---
+++ src/main.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <filesystem>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+namespace fs = std::filesystem;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main(int argc, char* argv[]) {
+
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return 1;
+    }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
+
+    // required for Mac OS X
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    const auto window_width = 1280;
+    const auto window_height = 720;
+    GLFWwindow* window = glfwCreateWindow(window_width, window_height, "kekw", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    GLFWimage images[1];
+
+    static auto exe_path = argv[0];
+    static auto icon_path = fs::path(exe_path).parent_path() / "app-icon.png";
+
+    // TODO: this fails for non-ascii paths, converting from wide string to string seems like a total ball ache.
+    // needs <windows.h> to be included and the WideCharToMultiByte should be called.
+    images[0].pixels = stbi_load(
+        icon_path.string().c_str(), 
+        &images[0].width, 
+        &images[0].height, 
+        0, 
+        4); //rgba channels 
+
+    glfwSetWindowIcon(window, 1, images); 
+    stbi_image_free(images[0].pixels);
+
+    if (!gladLoadGL())
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return 1;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    ImGui::StyleColorsDark();
+
+    auto SCALE = 2.0f;
+    ImFontConfig cfg;
+    cfg.SizePixels = 13 * SCALE;
+    ImGui::GetIO().Fonts->AddFontDefault(&cfg)->DisplayOffset.y = SCALE;
+    ImGui::GetStyle().ScaleAllSizes(SCALE);
+    ImGui::GetIO().FontGlobalScale = SCALE / 2;
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330 core");
+
+    bool show_demo_window = true;
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
